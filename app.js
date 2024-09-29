155f9e97e70c9e6255b6822d3a0760447695d834
ariesbhy <basmahfarhan97@icloud.com>
2024-09-29 12:13:24 +0300

    finished from create first route

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const products = require("./data");
 const app = express();
 
+// create first routes
+app.get("/", (req, res) => {
+  return res.json({ message: "Basmah" });
+});
+
+app.get("/api/products", (req, res) => {
+  return res.status(200).json({ data: data });
+});
 app.listen(8000, () => {
-  console.log("It's works");
+  console.log("Server started on port 8000");
 });
---
+++ data.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: 1, name: "chocolate" },
+  { id: 2, name: "chips" },
+  { id: 3, name: "nuts" },
+];
+
+module.exports = products;
